996088f87024740b4111792304f4da0e92ccf822
Xopher-11 <258705265+Xopher-11@users.noreply.github.com>
2026-03-16 09:22:52 -0400

    Completados los servicios de integración, auditoría y DI

---
+++ PRORC.Infrastructure/DependencyInjection/InfrastructureDI.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PRORC.Domain.Interfaces.Integrations;
+using PRORC.Domain.Interfaces.Logging;
+using PRORC.Infrastructure.Integrations.Notifications;
+using PRORC.Infrastructure.Integrations.Payments;
+using PRORC.Infrastructure.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRORC.Infrastructure.DependencyInjection
+{
+    public static class InfrastructureDI
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<INotificationSender, EmailNotificationService>();
+            services.AddScoped<IPaymentGateway, PaymentGatewayService>();
+            services.AddScoped<IAuditLogger, AuditLogger>();
+
+            return services;
+        }
+    }
+}
--- PRORC.Infrastructure/Integrations/Notifications/EmailNotificationService.cs
+++ PRORC.Infrastructure/Integrations/Notifications/EmailNotificationService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using PRORC.Domain.Entities.Notifications;
+using PRORC.Domain.Interfaces.Integrations;
+using PRORC.Domain.Interfaces.Logging;
+using PRORC.Domain.Interfaces.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,30 @@ using System.Threading.Tasks;
 
 namespace PRORC.Infrastructure.Integrations.Notifications
 {
-    internal class EmailNotificationService
+    public class EmailNotificationService : INotificationSender
     {
+        private readonly INotificationRepository _notificationRepository;
+        private readonly IAuditLogger _auditLogger;
+
+        public EmailNotificationService(INotificationRepository notificationRepository, IAuditLogger auditLogger)
+        {
+            _notificationRepository = notificationRepository;
+            _auditLogger = auditLogger;
+        }
+
+        public async Task SendAsync(int userId, string title, string message)
+        {
+            var notification = new Notification
+            {
+                UserId = userId,
+                Title = title,
+                Message = message,
+                IsRead = false,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            await _notificationRepository.AddAsync(notification);
+            await _auditLogger.LogAsync("NotificationSent", $"Notification stored for user {userId} with title '{title}'");
+        }
     }
 }
